ffaa89dd8dddc2b79691e2f0af7799961dfed18f
Lazyhacker25 <88123226+Lazyhacker25@users.noreply.github.com>
2021-09-07 08:41:10 +0530

    Update ping.py

--- VictoriaMusic/modules/ping.py
+++ VictoriaMusic/modules/ping.py
@@ -1,5 +1,38 @@
 import time
 from datetime import datetime
+import os
+from os import path
+from typing import Callable
+
+import aiofiles
+import aiohttp
+import ffmpeg
+import requests
+import wget
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from Python_ARQ import ARQ
+from youtube_search import YoutubeSearch
+
+from VictoriaMusic.config import ARQ_API_KEY
+from VictoriaMusic.config import BOT_NAME as bn
+from VictoriaMusic.config import DURATION_LIMIT
+from VictoriaMusic.config import UPDATES_CHANNEL as updateschannel
+from VictoriaMusic.config import que
+from VictoriaMusic.function.admins import admins as a
+from VictoriaMusic.helpers.admins import get_administrators
+from VictoriaMusic.helpers.channelmusic import get_chat_id
+from VictoriaMusic.helpers.decorators import authorized_users_only
+from VictoriaMusic.helpers.filters import command, other_filters
+from VictoriaMusic.helpers.gets import get_file_name
+from VictoriaMusic.services.callsmusic import callsmusic
+from VictoriaMusic.services.callsmusic import client as USER
+from VictoriaMusic.services.converter.converter import convert
+from VictoriaMusic.services.downloaders import youtube
+from VictoriaMusic.services.queues import queues
+
 @Client.on_message(filters.command('ping', '!'))
 async def ping_msg_handler(_, m: Message):
     to_be_edited = await m.reply('Pinging..')
